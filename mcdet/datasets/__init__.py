e684b3acccc6f34d4276ff2644c5771c90c4143a
jemo_maeng <maengjemo@gm.gist.ac.kr>
2025-10-11 20:04:31 +0000

    Update : config

--- mcdet/datasets/__init__.py
+++ mcdet/datasets/__init__.py
@@ -1,4 +1,6 @@
 # mcdet/datasets/__init__.py
+
+# 1. 모든 데이터셋 클래스들을 임포트합니다.
 from .custom_flir_dataset import FLIRCatDataset, FLIRCatDataset2
 from .custom_drone_Dataset import GISTDataset
 from .kaist_rgbt_coco_dataset import KaistRgbtCocoDataset
@@ -8,8 +10,12 @@ from .custom_deliver_detection_dataset import DELIVERDetectionDataset
 from .custom_sampler import CustomSampler
 from .custom_sejong_detection_dataset import SejongDetectionDataset
 
-__all__ =[
-    'FLIRCatDataset', 'FLIRCatDataset2', 'GISTDataset', 'KaistRgbtCocoDataset', 
-    'LLVIPRgbtDataset', 'FLIRRgbtCocoDataset', 'DELIVERDetectionDataset', 
-     'CustomSampler', 'SejongDetectionDataset'
-]
+# 2. transforms 서브모듈을 임포트하여 모든 커스텀 transform을 등록합니다.
+from .transforms import * # <--- 이 줄을 추가하세요!
+
+# 3. __all__을 정의할 때, 데이터셋 클래스들과 transforms의 클래스들을 모두 포함시킵니다.
+__all__ = [
+    'FLIRCatDataset', 'FLIRCatDataset2', 'GISTDataset', 'KaistRgbtCocoDataset',
+    'FLIRRgbtCocoDataset', 'DELIVERDetectionDataset',
+    'CustomSampler', 'SejongDetectionDataset'
+] + transforms.__all__  
